4405448f1070e974b1a269fa1c275a5fb5e6bc56
rookiepeng <rookiepeng@gmail.com>
2020-03-21 10:23:18 -0400

    use template to export

---
+++ export_html.bat
@@ -0,0 +1,17 @@
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/doppler_radar.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/fmcw_radar.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/fmcw_reflector_raytracing.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/lidar.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/multi_path_raytracing.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/pmcw_radar.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/rcs_raytracing.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/roc.ipynb
+
+jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/tdm_mimo_fmcw_radar.ipynb
