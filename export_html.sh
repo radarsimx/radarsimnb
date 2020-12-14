41271bf4195202752ccacc8c74ca92f2f4641264
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2020-12-14 11:10:05 -0500

    update export scripts
    Former-commit-id: 040fc02ce2ca71af9bb5cd13e799f1b106abab40

--- export_html.sh
+++ export_html.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/doppler_radar.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/doppler_radar.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/fmcw_radar.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/fmcw_radar.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/fmcw_reflector_raytracing.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/fmcw_reflector_raytracing.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/lidar.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/lidar.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/multi_path_raytracing.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/multi_path_raytracing.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/pmcw_radar.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/pmcw_radar.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/rcs_raytracing.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/rcs_raytracing.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/roc.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/roc.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/tdm_mimo_fmcw_radar.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/tdm_mimo_fmcw_radar.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/doppler_rotating_target_raytracing.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/doppler_rotating_target_raytracing.ipynb
 
-jupyter nbconvert --output-dir='./html' --template my_template.tpl --to html notebooks/isar_raytracing.ipynb
+jupyter nbconvert --output-dir=./html --to html notebooks/isar_raytracing.ipynb
